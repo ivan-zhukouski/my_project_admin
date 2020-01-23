837048cae13ba4ab0a8fe7a045e3d3f637e256dd
ivan-zhukouski <stigmatix.by@gmail.com>
2020-01-24 00:35:56 +0300

    updated kiosk page, ApiService

--- src/app/pages/main/pages/some-kiosk/some-kiosk.component.ts
+++ src/app/pages/main/pages/some-kiosk/some-kiosk.component.ts
@@ -12,6 +12,12 @@ export class SomeKioskComponent implements OnInit {
   kiosk: SomeKiosk;
   constructor(public apiService: ApiService, private route: ActivatedRoute) {
   }
+  public hours = [
+    {title: '1'}, {title: '2'}, {title: '3'}, {title: '4'}, {title: '5'}, {title: '6'},
+    {title: '7'}, {title: '8'}, {title: '9'}, {title: '10'}, {title: '11'}, {title: '12'},
+    {title: '13'}, {title: '14'}, {title: '15'}, {title: '16'}, {title: '17'}, {title: '18'},
+    {title: '19'}, {title: '20'}, {title: '21'}, {title: '22'}, {title: '23'}, {title: '24'},
+  ];
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.apiService.getSomeKiosk(params.id)
